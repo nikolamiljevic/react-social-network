587bc185554d20f7f34791ffb370566047939b0d
Nikola <milje18@gmail.com>
2020-10-17 12:15:01 +0200

    create,update and delete event has been made and view of the events modified

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, Icon, Item, List, Segment } from 'semantic-ui-react'
 import EventListAttendee from './EventListAttendee'
 
-export default function EventListItem({event}) {
+export default function EventListItem({event, selectEvent, deleteEvent}) {
     return (
         <Segment.Group>
            
@@ -37,7 +37,8 @@ export default function EventListItem({event}) {
 
             <Segment clearing>
               <div>{event.description}</div>
-              <Button color="teal" floated="right" content="View" />
+              <Button color="red" floated="right" content="Delete" onClick={() => deleteEvent(event.id)}/>
+              <Button color="teal" floated="right" content="View" onClick={() => selectEvent(event)}/>
             </Segment>
 
         </Segment.Group>
