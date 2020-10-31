4b1d8f572a136eb731aaa288c32771762c298436
Nikola <milje18@gmail.com>
2020-10-31 12:34:20 +0100

    redux thunk added, loading indicators,toast notification, loading component and event filters component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,5 +1,6 @@
 import React, {Fragment} from 'react';
 import { Route, useLocation } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
 import EventDetailedPage from '../../features/events/eventDetailed/EventDetailedPage';
@@ -16,6 +17,10 @@ function App() {
   return (
       <Fragment>
         <ModalManager/>
+        <ToastContainer
+          position="bottom-right"
+          hideProgressBar
+        />
         <Route exact path="/" component={HomePage}/>
         <Route path={'/(.+)'} render={() => (
           <>
--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Button } from 'semantic-ui-react';
 import { openModal } from '../../app/commom/modals/modalReducer';
 import { decrement, increment } from './testReducer';
 
 export default function Sandbox() {
-    const dispatch = useDispatch()
+
+    const dispatch = useDispatch();
+    const [target,setTarget] = useState(null);
     const data = useSelector(state=> state.test.data);
+    const {loading} = useSelector(state => state.async);
 
     return (
         <div>
         <h1>Testing 123</h1>
         <h3>The data is: {data} </h3>
         <Button 
-            onClick = {() => dispatch(increment(10))}
+            name = "increment"
+            loading={loading && target == 'increment'}
+            onClick = {(e) => {
+                dispatch(increment(10));
+                setTarget(e.target.name)
+            }}
             content="Increment" 
             color="green"
         />
         <Button 
-            onClick = {() => dispatch(decrement(5))}
+            name="decrement"
+            loading={loading && target == 'decrement'}
+            onClick = {(e) => {
+                dispatch(decrement(5));
+                setTarget(e.target.name)
+            }}
             content="Decrement" 
             color="red"
         />
